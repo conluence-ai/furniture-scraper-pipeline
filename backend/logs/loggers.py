230fad2966d199f115fb4798ab345e5c34223c66
Aditya15267 <aditya301097@gmail.com>
2025-07-24 16:24:38 +0530

    Add logging configuration

---
+++ backend/logs/loggers.py
@@ -0,0 +1,18 @@
+import logging
+
+def validationLoggerSetup():
+    """
+        Set up the logger for validation logs.
+        
+        This function configures the logging settings for the validation module,
+        including the log file name, logging level, format, and date format.
+    """
+    
+    logging.basicConfig(
+        filename="backend/logs/scraper_logs.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+logger = logging.getLogger(__name__)
