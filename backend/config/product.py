79dd23d54ae0694ef7300a90634fc5637438ac7b
Aditya15267 <aditya301097@gmail.com>
2025-07-28 15:20:56 +0530

    Added Product information that needs to be extracted

---
+++ backend/config/product.py
@@ -0,0 +1,13 @@
+# Import necessary libraries
+from typing import List
+from dataclasses import dataclass
+
+@dataclass
+class Product:
+    """Data class to represent a furniture product"""
+    productName: str
+    description: str
+    productUrl: str
+    designerName: str
+    imageUrls: List[str]
+    category: str
